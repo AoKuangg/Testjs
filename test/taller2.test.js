e2338a9e5a4a26163c69d686d58999d56dee0928
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-09-02 10:01:06 -0500

    feat: :sparkles: Se agrega los test para probar la logica del taller

---
+++ test/taller2.test.js
@@ -0,0 +1,87 @@
+import {
+  validarBeca,
+  personaMayor,
+  parEimpar,
+  calcular,
+  voltaje,
+  cantidadEstudiantes,
+} from "../src/taller.js";
+
+//Beca
+describe("1. Validar si un estudiante aprobo o no una beca", () => {
+  let resNota = validarBeca(3, 4, 3);
+  let array = ["No paso", "Si paso"];
+  test("Verifica si el resultado esta entre las dos opciones solicitadas", () => {
+    let res = array.includes(resNota);
+    expect(res).toBe(true);
+  });
+  test("Verficar si es un stringa", () => {
+    expect(typeof resNota).toBe("string");
+  });
+});
+
+//Par O Impar
+describe("2. Validar si es par o impar y si es mayor o menor que 10", () => {
+  test("Retorna par y mayor/menor que 10", () => {
+    expect(parEimpar(18)).toContain("El resultado es par y mayor a 10");
+    expect(parEimpar(6)).toContain("El resultado es par y menor a  10");
+  });
+  it("Retorna que es impar y mayor/menor que 10", () => {
+    expect(parEimpar(15)).toContain("El resultado es impar y mayor a 10");
+    expect(parEimpar(9)).toContain("El resultado es impar y menor a  10");
+  });
+});
+
+//Voltaje
+describe("3. Calcular voltaje de la corriente dada", () => {
+  it("Verifica que retorne un valor númerico", () => {
+    expect(typeof voltaje()).toBe("number");
+  });
+
+  it("Retorna el valor (6.5 * 5) = 32.5", () => {
+    expect(voltaje(6.5, 5)).toBe(32.5);
+  });
+});
+
+//personaMayor
+describe("4. Determinar a la persona mayor", () => {
+  let personas = [
+    { nombre: "Jhon", edad: 40 },
+    { nombre: "Carlos", edad: 30 },
+    { nombre: "Camilo", edad: 19 },
+  ];
+  it("Validar si retorna un objeto", () => {
+    expect(typeof personaMayor(personas)).toBe(`object`);
+  });
+
+  it("Retorna los datos de la persona mayor", () => {
+    expect(personaMayor(personas)).toEqual({ nombre: "Jhon", edad: 40 });
+  });
+});
+
+//Calcular
+describe("5. Hacer operaciones matematicas a dos valores", () => {
+  it("retorna valores calculados según sus valores", () => {
+    expect(calcular(4, 2)).toEqual([6, 2]);
+    expect(calcular(2, 4)).toEqual([8, 0.5]);
+    expect(calcular(2, 2)).toEqual([2, 2]);
+  });
+});
+
+//cantidadEstudiantes
+describe("6. Obtener la informacion a cerca de los distintos estudiantes", () => {
+  let estudiantesArray = [
+    { nombre: "Camilo", sexo: "M", nota: 4.2 },
+    { nombre: "Rosa", sexo: "F", nota: 4.9 },
+    { nombre: "Nancy", sexo: "F", nota: 3.5 },
+  ];
+
+  it("Validar que retorne un string", () => {
+    expect(typeof cantidadEstudiantes(estudiantesArray)).toBe("string");
+  });
+  it("Validar que retorne al mejor estudiante", () => {
+    expect(cantidadEstudiantes(estudiantesArray)).toBe(
+      "El mejor estudiante fue: Rosa, hay 2 estudiantes femeninos y 1 masculinos."
+    );
+  });
+});
